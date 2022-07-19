f9726cf99cd8df1f309af3c8e3610fb425833a1e
GHOST <ghostdevbusiness@gmail.com>
2022-07-19 01:41:44 +0100

    feat: worker

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 export interface Env {
-
+    IMAGINE: R2Bucket;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+    async fetch(
+        request: Request,
+        env: Env,
+        ctx: ExecutionContext,
+    ): Promise<Response> {
+        const file = await env.IMAGINE.get('imagine.gif');
+
+        if (!file)
+            return new Response('Not found', {
+                status: 404,
+            });
+
+        return new Response(await file.blob(), {
+            headers: {
+                'content-type': file.httpMetadata.contentType!,
+            },
+        });
+    },
 };
