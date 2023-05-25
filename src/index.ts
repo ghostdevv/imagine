795b615a4325339fef42d6dfe3e1a08ab4340336
GHOST <ghostdevbusiness@gmail.com>
2023-05-25 17:02:22 +0000

    feat: work in progress renderer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
+import puppeteer, { type BrowserWorker } from '@cloudflare/puppeteer';
+
 export interface Env {
     IMAGINE: R2Bucket;
+    BROWSER: BrowserWorker;
+}
+
+function parseQuery(query: any) {
+    if (typeof query != 'string' || query?.trim()?.length == 0) {
+        return { key: 'imagine.gif', name: 'imagine' };
+    }
+
+    const name = query
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9!? ]/gm, '');
+
+    if (name == 'imagine') {
+        return { key: 'imagine.gif', name: 'imagine' };
+    }
+
+    const key = `generated/${name.replace(/ /gm, '-')}.gif`;
+
+    return { key, name };
 }
 
 export default {
@@ -8,17 +30,39 @@ export default {
         env: Env,
         ctx: ExecutionContext,
     ): Promise<Response> {
-        const file = await env.IMAGINE.get('imagine.gif');
+        const url = new URL(request.url);
 
-        if (!file)
-            return new Response('Not found', {
-                status: 404,
+        if (url.pathname == '/base') {
+            const file = await env.IMAGINE.get('base.gif');
+
+            return new Response(file?.body, {
+                headers: {
+                    'Content-Type': 'image/gif',
+                },
+            });
+        }
+
+        const { key, name } = parseQuery(url.searchParams.get('q'));
+        const file = await env.IMAGINE.get(key);
+
+        if (file)
+            return new Response(file.body, {
+                headers: {
+                    'Content-Type': 'image/gif',
+                },
             });
 
-        return new Response(await file.blob(), {
-            headers: {
-                'content-type': file.httpMetadata.contentType!,
-            },
-        });
+        // const browser = await puppeteer.launch(env.BROWSER);
+
+        // const [page] = await browser.pages();
+        // await page.goto('https://qifi.org/');
+        // await page.type('#ssid', 'homebase');
+        // await page.type('#key', 'tubulargagy');
+        // await page.click('#generate');
+
+        // const res = await page.$eval('#qrcode canvas', (el) => el.toDataURL());
+        // console.log(res);
+
+        return new Response('asd');
     },
 };
